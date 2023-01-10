7addf3a7bf0a8546facf4d0b04dde3809a15cff8
Justin Wolcott <Justin.W.Wolcott@gmail.com>
2023-01-10 03:34:10 -0600

    update to expose ApplessBrowser directly in export

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 import ApplessLocal from "./js/browser/index.js";
+import ApplessBrowser from "./js/browser/browser.js";
 import ApplessMobile from "./js/mobile/index.js";
 
-export {ApplessLocal, ApplessMobile};
+export {ApplessLocal, ApplessMobile, ApplessBrowser};
